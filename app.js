12e7f3fd9d7a305721fe6d318d6bad671c2e0c16
john <johnhong.learn@gmail.com>
2022-01-15 21:42:20 -0500

    Completed basic CRUD requirements

--- app.js
+++ app.js
@@ -10,61 +10,66 @@ app.use(express.static(__dirname + "/assets"));
 
 const connection = require('./db/connection.js');
 
+//we will use this variable as our backend cache
+let data;
 
 //Landing page, loading/Reading in table data
 app.get("/", (req, res) => {	
 	const query = "select * from inventory;";
 	connection.query(query, function(err, results){
 		if(err) throw err;
-		res.render("landingPage", {data : results});
+		//storing the first retrieved variables
+		data = results;
+		res.render("landingPage", {data});
 	});
 })
 
+// Editing/Deleting a product, using POST request with row num to pass to backend to check cache
+app.post("/", (req, res) =>{
+	let product;
+	//catching a post request from front end and rendering the edit product page
+	if(req.body.editReq != undefined){
+		product = data[parseInt(req.body.editReq)]
+		res.render( "editProduct", {product});
+	}else if(req.body.sendProdData != undefined){
+		updatedProduct = {
+			product_name: req.body.productName,
+			quantity: parseInt(req.body.quantity)
+		};
+		originalProduct = {
+			id : parseInt(req.body.id)
+		};
+		console.log(originalProduct);
+		connection.query("update inventory set ? where ?", [updatedProduct, originalProduct], function(err, results){
+			if(err) throw err;
+			res.redirect("/");
+		})
+	}
+	else{
+		//otherwise we are deleting the product, only 2 possible ways to get a post request
+		product = data[parseInt(req.body.delete)]
+		connection.query("delete from inventory where id = ?", product.id, function(err, result){
+			if(err) throw err;
+			res.redirect("/")
+		})
+	}
+})
 
 //For if the user clicks on the hyperlink to insert a product
 app.get("/insert", (req, res) =>{
-	res.render("insertProduct");
+        res.render("insertProduct");
 })
 
 
 // Creating/Inserting a new product
 app.post("/insert", (req, res) => {
-	const product ={
-		product_name : req.body.productName,
-		quantity : req.body.quantity
-        };	
-	connection.query("insert into inventory set ?", product, function(err, result){
+        const product = [req.body.productName, parseInt(req.body.quantity)]
+        connection.query("insert into inventory (product_name, quantity) values(?, ?)", product, function(err, result){
                 if(err) throw err;
                 res.redirect("/");
         });
-	
 })
 
 
-// Updating/editing a product
-app.post("/", (req, res) =>{
-	const product = {
-		id : req.body.id,
-		product_name : req.body.product_name,
-		quantity : req.body.quantity,
-	};
-	const prodChosen = [product, product.id]
-	connection.query("update inventory set ? where id = ?", prodChosen, function(err, result){
-		if(err) throw err;
-		res.redirect("/");
-	})
-})
-
-//Deleting a product
-app.delete('/', (req, res) =>{
-	const product ={
-		id : req.body.id
-	};
-	connection.query("delete from inventory where id = ?", prod, function(err, result){
-		if(err) throw err;
-		res.redirect("/");
-	})
-	//do nothing if user confirms that they do not want to delete
-})
 
 app.listen(port, () => {console.log("connection successful")});
